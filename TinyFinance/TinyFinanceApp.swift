00c2311fa213024df81646d109761827d466eb50
n0lit <n0rthl1ght@yandex.ru>
2026-01-07 00:11:38 +0300

    Initial Commit

---
+++ TinyFinance/TinyFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyFinanceApp.swift
+//  TinyFinance
+//
+//  Created by Максим Фурсов on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct TinyFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
